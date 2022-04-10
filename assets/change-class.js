4bafbfea4771919c5a5792f76f1a845023e54e15
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-04-10 01:52:13 -0400

    Update change-class.js

--- assets/change-class.js
+++ assets/change-class.js
@@ -1,3 +1,4 @@
+// Upload Images From Local 
 $(function () {
     $(":file").change(function () {
         if (this.files && this.files[0]) {
@@ -12,7 +13,7 @@ function imageIsLoaded(e) {
     $('#myImg').attr('src', e.target.result);
 };
 
-// Filter Function HTML
+// Filter Function(s) HTML
 
 "use strict";
 
@@ -38,3 +39,55 @@ $(document).on("change", "#selector", function () {
   $("figure").addClass(newClassName);
   prevClassName = newClassName;
 });
+
+
+// Image from url 
+
+var Ximgurl = $('#try').val();
+var XimgDefault = $('#def').val();    
+
+    loadImage(Ximgurl,XimgDefault)
+
+
+$('#reload').click(function() {
+    Ximgurl = $('#try').val();
+    XimgDefault = $('#def').val();
+    loadImage(Ximgurl,XimgDefault);
+});
+
+function loadImage(imgurl,imgDefault) {
+    var img = $('#myImg');
+
+    // wrap our new image in jQuery, then:
+    $(img)
+    // once the image has loaded, execute this code
+    .load(function () {
+        // set the image hidden by default    
+        $(this).hide();
+
+        // with the holding div #loader, apply:
+        $('#loader')
+        // remove the loading class (so no background spinner), 
+        .removeClass('loading')
+        // then insert our image
+        .append(this);
+
+        // fade our image in to create a nice effect
+        $(this).fadeIn();
+    })
+
+    // if there was an error loading the image, react accordingly
+    .error(function () {
+        // notify the user that the image could not be loaded
+        $('#loader').removeClass('loading')
+        $(img).attr('src', imgDefault);
+
+    })
+
+    // *finally*, set the src attribute of the new image to our image
+    .attr('src', imgurl);
+}
+
+
+
+
